bacfcd680b3292fdbf2969aea55b4384a9db974e
eduardoportillo <eportillovirreira@gmail.com>
2023-05-24 09:14:52 -0400

    delete

--- .github/actions/ios_export_ipa.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. $GITHUB_WORKSPACE/.env
-xcodebuild -exportArchive \
-    -archivePath $GITHUB_WORKSPACE/ios/Actions.xcarchive \
-    -exportOptionsPlist $GITHUB_WORKSPACE/ios/ExportOptions.plist \
-    -exportPath $RUNNER_TEMP/export \
-    -allowProvisioningUpdates
